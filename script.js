184e88bda22ae522be1918a547bfa268e02ff09c
JacobForsman <43602729+JacobForsman@users.noreply.github.com>
2019-08-22 09:06:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let myButton = document.getElementById("button");
+let myText = ("Hello World!")
+
+
+myButton.addEventListener('click', onButtonClicked);
+
+function onButtonClicked() {
+    alert (myText)
+}
